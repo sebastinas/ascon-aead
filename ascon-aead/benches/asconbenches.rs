be140e3a958dba70860c7f8d6fa84377a4cf3059
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2022-06-02 18:36:35 +0200

    Use random keys, nonces, and plaintexts for benchmarks

--- ascon-aead/benches/asconbenches.rs
+++ ascon-aead/benches/asconbenches.rs
@@ -1,50 +1,65 @@
 use ascon_aead::{
     aead::{generic_array::typenum::Unsigned, Aead, AeadInPlace, NewAead},
-    Ascon128, Ascon128a, Ascon80pq, Key, Nonce,
+    Ascon128, Ascon128a, Ascon80pq,
 };
 use criterion::{
     black_box, criterion_group, criterion_main, Bencher, BenchmarkId, Criterion, Throughput,
 };
+use rand::{rngs::StdRng, RngCore, SeedableRng};
 
 const KB: usize = 1024;
 
-fn bench_for_size<A: NewAead + Aead>(b: &mut Bencher, size: usize) {
-    let cipher = A::new(Key::<A>::from_slice(
-        &b"very secret key.0123"[..A::KeySize::USIZE],
-    ));
-    let nonce = Nonce::<A>::from_slice(b"unique nonce 012");
-    let plaintext = vec![0u8; size];
+fn bench_for_size<A: NewAead + Aead>(b: &mut Bencher, rng: &mut dyn RngCore, size: usize) {
+    let mut key = vec![0u8; A::KeySize::USIZE];
+    rng.fill_bytes(key.as_mut_slice());
+    let mut nonce = vec![0u8; A::NonceSize::USIZE];
+    rng.fill_bytes(nonce.as_mut_slice());
+    let mut plaintext = vec![0u8; size];
+    rng.fill_bytes(plaintext.as_mut_slice());
 
-    b.iter(|| black_box(cipher.encrypt(nonce, plaintext.as_slice()).unwrap()));
+    let cipher = A::new(key.as_slice().into());
+    let nonce = nonce.as_slice().into();
+
+    b.iter(|| black_box(cipher.encrypt(nonce, plaintext.as_slice())));
 }
 
-fn bench_for_size_inplace<A: NewAead + AeadInPlace>(b: &mut Bencher, size: usize) {
-    let cipher = A::new(Key::<A>::from_slice(
-        &b"very secret key.0123"[..A::KeySize::USIZE],
-    ));
-    let nonce = Nonce::<A>::from_slice(b"unique nonce 012");
+fn bench_for_size_inplace<A: NewAead + AeadInPlace>(
+    b: &mut Bencher,
+    rng: &mut dyn RngCore,
+    size: usize,
+) {
+    let mut key = vec![0u8; A::KeySize::USIZE];
+    rng.fill_bytes(key.as_mut_slice());
+    let mut nonce = vec![0u8; A::NonceSize::USIZE];
+    rng.fill_bytes(nonce.as_mut_slice());
     let mut buffer = vec![0u8; size + 16];
+    rng.fill_bytes(buffer.as_mut_slice());
 
-    b.iter(|| black_box(cipher.encrypt_in_place(nonce, b"", &mut buffer).unwrap()));
+    let cipher = A::new(key.as_slice().into());
+    let nonce = nonce.as_slice().into();
+
+    b.iter(|| black_box(cipher.encrypt_in_place(nonce, b"", &mut buffer)));
 }
 
 fn criterion_benchmark<A: NewAead + Aead>(c: &mut Criterion, name: &str) {
+    let mut rng = StdRng::seed_from_u64(0x0123456789abcdef);
     let mut group = c.benchmark_group(name);
-    for size in [1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB].iter() {
+    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            bench_for_size::<A>(b, size)
+            bench_for_size::<A>(b, &mut rng, size)
         });
     }
     group.finish();
 }
 
 fn criterion_benchmark_inplace<A: NewAead + AeadInPlace>(c: &mut Criterion, name: &str) {
+    let mut rng = StdRng::seed_from_u64(0x0123456789abcdef);
     let mut group = c.benchmark_group(name);
-    for size in [1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB].iter() {
+    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            bench_for_size_inplace::<A>(b, size)
+            bench_for_size_inplace::<A>(b, &mut rng, size)
         });
     }
     group.finish();
