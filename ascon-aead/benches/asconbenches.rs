6006190ef401d8a26da269b6536c09a058f4aef5
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2022-06-02 18:14:44 +0200

    Use Ascon* instead of Parameters*

--- ascon-aead/benches/asconbenches.rs
+++ ascon-aead/benches/asconbenches.rs
@@ -1,6 +1,6 @@
 use ascon_aead::{
     aead::{generic_array::typenum::Unsigned, Aead, AeadInPlace, NewAead},
-    Ascon, Key, Nonce, Parameters, Parameters128, Parameters128a, Parameters80pq,
+    Ascon128, Ascon128a, Ascon80pq, Key, Nonce,
 };
 use criterion::{
     black_box, criterion_group, criterion_main, Bencher, BenchmarkId, Criterion, Throughput,
@@ -8,70 +8,70 @@ use criterion::{
 
 const KB: usize = 1024;
 
-fn bench_for_size<P: Parameters>(b: &mut Bencher, size: usize) {
-    let cipher = Ascon::<P>::new(Key::<Ascon<P>>::from_slice(
-        &b"very secret key.0123"[..P::KeySize::USIZE],
+fn bench_for_size<A: NewAead + Aead>(b: &mut Bencher, size: usize) {
+    let cipher = A::new(Key::<A>::from_slice(
+        &b"very secret key.0123"[..A::KeySize::USIZE],
     ));
-    let nonce = Nonce::<Ascon<P>>::from_slice(b"unique nonce 012");
+    let nonce = Nonce::<A>::from_slice(b"unique nonce 012");
     let plaintext = vec![0u8; size];
 
     b.iter(|| black_box(cipher.encrypt(nonce, plaintext.as_slice()).unwrap()));
 }
 
-fn bench_for_size_inplace<P: Parameters>(b: &mut Bencher, size: usize) {
-    let cipher = Ascon::<P>::new(Key::<Ascon<P>>::from_slice(
-        &b"very secret key.0123"[..P::KeySize::USIZE],
+fn bench_for_size_inplace<A: NewAead + AeadInPlace>(b: &mut Bencher, size: usize) {
+    let cipher = A::new(Key::<A>::from_slice(
+        &b"very secret key.0123"[..A::KeySize::USIZE],
     ));
-    let nonce = Nonce::<Ascon<P>>::from_slice(b"unique nonce 012");
+    let nonce = Nonce::<A>::from_slice(b"unique nonce 012");
     let mut buffer = vec![0u8; size + 16];
 
     b.iter(|| black_box(cipher.encrypt_in_place(nonce, b"", &mut buffer).unwrap()));
 }
 
-fn criterion_benchmark<P: Parameters>(c: &mut Criterion, name: &str) {
+fn criterion_benchmark<A: NewAead + Aead>(c: &mut Criterion, name: &str) {
     let mut group = c.benchmark_group(name);
     for size in [1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            bench_for_size::<P>(b, size)
+            bench_for_size::<A>(b, size)
         });
     }
     group.finish();
 }
 
-fn criterion_benchmark_inplace<P: Parameters>(c: &mut Criterion, name: &str) {
+fn criterion_benchmark_inplace<A: NewAead + AeadInPlace>(c: &mut Criterion, name: &str) {
     let mut group = c.benchmark_group(name);
     for size in [1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            bench_for_size_inplace::<P>(b, size)
+            bench_for_size_inplace::<A>(b, size)
         });
     }
     group.finish();
 }
 
 fn criterion_bench_ascon128(c: &mut Criterion) {
-    criterion_benchmark::<Parameters128>(c, "Ascon-128");
+    criterion_benchmark::<Ascon128>(c, "Ascon-128");
 }
 
 fn criterion_bench_ascon128a(c: &mut Criterion) {
-    criterion_benchmark::<Parameters128a>(c, "Ascon-128a");
+    criterion_benchmark::<Ascon128a>(c, "Ascon-128a");
 }
 
 fn criterion_bench_ascon80pq(c: &mut Criterion) {
-    criterion_benchmark::<Parameters80pq>(c, "Ascon-80pq");
+    criterion_benchmark::<Ascon80pq>(c, "Ascon-80pq");
 }
 
 fn criterion_bench_ascon128_inplace(c: &mut Criterion) {
-    criterion_benchmark_inplace::<Parameters128>(c, "Ascon-128 (inplace)");
+    criterion_benchmark_inplace::<Ascon128>(c, "Ascon-128 (inplace)");
 }
 
 fn criterion_bench_ascon128a_inplace(c: &mut Criterion) {
-    criterion_benchmark_inplace::<Parameters128a>(c, "Ascon-128a (inplace)");
+    criterion_benchmark_inplace::<Ascon128a>(c, "Ascon-128a (inplace)");
 }
 
 fn criterion_bench_ascon80pq_inplace(c: &mut Criterion) {
-    criterion_benchmark_inplace::<Parameters80pq>(c, "Ascon-80pq (inplace)");
+    criterion_benchmark_inplace::<Ascon80pq>(c, "Ascon-80pq (inplace)");
 }
 
 criterion_group!(
